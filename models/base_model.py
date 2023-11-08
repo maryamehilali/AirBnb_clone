8650806fc826bb829313597de9425dee14982c5c
IBRAHIMAlla <brahimalla973@gmail.com>
2023-11-08 13:33:20 -0800

    base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -19,3 +19,12 @@ class BaseModel:
     def save(self):
         """ This method updates the updated_at attribute with the current datetime. """
         self.updated_at = datetime.now()
+
+    def to_dict(self):
+        """returns a dictionary containing all keys/vlues
+        of __dict__ of the instance"""
+        my_dict = self.__dict__.copy()
+        my_dict['__class__'] = self.__class__.__name__
+        my_dict['created_at'] = self.created_at.isoformat()
+        my_dict['updated_at'] = self.updated_at.isoformat()
+        return my_dict
